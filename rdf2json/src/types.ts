0d296d3346c7e27974ba110066b0cfd11bcf4ecf
Chris Newton <me@chrisnewtn.com>
2025-03-08 11:33:18 +0000

    Add support for optional marc546 field

--- rdf2json/src/types.ts
+++ rdf2json/src/types.ts
@@ -145,6 +145,21 @@ export interface FormattedEbook {
    */
   marc520?: string;
 
+  /**
+   * 546 - Language Note (R)
+   *
+   * Textual information on the language or notation system used to convey the
+   * content of the described materials. A description of the alphabet, script,
+   * or other symbol system (e.g., arabic alphabet, ASCII, bar code,
+   * logarithmic graphing) may also be included, but for musical notation system
+   * terms, prefer field 348 (Notated Music Characteristics). Coded language
+   * information is contained in fields 008/35-37 (Language) and/or
+   * 041 (Language code).
+   *
+   * @see {@link https://www.loc.gov/marc/bibliographic/bd546.html}
+   */
+  marc546?: string;
+
   marc901?: string[];
 
   /**
@@ -434,6 +449,7 @@ export const formattedEbookSchema: JTDSchemaType<FormattedEbook, {
       elements: { type: 'string' },
     },
     marc520: { type: 'string' },
+    marc546: { type: 'string' },
     marc901: {
       elements: { type: 'string' },
     },
