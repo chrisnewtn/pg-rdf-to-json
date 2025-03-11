8de3d3ee095f9272ddbb3bb0d132f23f136508f1
Chris Newton <me@chrisnewtn.com>
2025-03-11 16:43:06 +0000

    test: centralize and speed up the tests

--- test/pg10202.test.js
+++ test/pg10202.test.js
@@ -1,34 +1,3 @@
-import { booksFromStream } from '../dist/index.js';
-import { before, describe, it } from 'node:test';
-import { equal, deepEqual } from 'node:assert/strict';
-import { createReadStream } from 'node:fs';
-import path from 'node:path';
-import { Ajv } from 'ajv/dist/jtd.js';
-import { formattedEbookSchema } from '../dist/types.js';
-import { extractProp } from '../dist/util.js';
+import { generateSchemaValidationSuite, getFixtureId } from './shared.js';
 
-const id = '10202';
-const file = `pg${id}.rdf`;
-
-const pathToFixture = path.resolve(import.meta.dirname, 'fixtures', file);
-
-describe(file, () => {
-  /** @type {import('../dist/types.js').FormattedEbook} */
-  let book;
-
-  before(async () => {
-    for await (const b of booksFromStream(createReadStream(pathToFixture))) {
-      book = b;
-    }
-  });
-
-  it('is a valid ebook', () => {
-    const ajv = new Ajv();
-    const validate = ajv.compile(formattedEbookSchema);
-    validate(book);
-    deepEqual((validate.errors || []).map(error => ({
-      ...error,
-      value: extractProp(book, error.instancePath)
-    })), []);
-  });
-});
+generateSchemaValidationSuite(getFixtureId(import.meta.filename));
